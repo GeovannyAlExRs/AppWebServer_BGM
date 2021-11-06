01f4368bef9371c6b92472ce6ce9dc9baf76d5df
Geovanny AlEx Rs <axel94bsc@gmail.com>
2021-11-05 22:42:29 -0500

    Commit Inicial subir proyecto

---
+++ AppWebServer_BGM/src/main/java/com/ec/busgeomap/web/app/AppWebServerBgmApplication.java
@@ -0,0 +1,13 @@
+package com.ec.busgeomap.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebServerBgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebServerBgmApplication.class, args);
+	}
+
+}
---
+++ AppWebServer_BGM/src/test/java/com/ec/busgeomap/web/app/AppWebServerBgmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec.busgeomap.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebServerBgmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

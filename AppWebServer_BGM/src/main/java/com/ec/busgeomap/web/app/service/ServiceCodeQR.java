68a6ac9efcbffded62cce3900947c33bd472ac8e
Geovanny AlEx Rs <axel94bsc@gmail.com>
2021-12-28 00:20:08 -0500

    Genera la IMG QR pero no sube al Storage

--- AppWebServer_BGM/src/main/java/com/ec/busgeomap/web/app/service/ServiceCodeQR.java
+++ AppWebServer_BGM/src/main/java/com/ec/busgeomap/web/app/service/ServiceCodeQR.java
@@ -1,8 +1,17 @@
 package com.ec.busgeomap.web.app.service;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.commons.lang3.RandomStringUtils;
@@ -18,7 +27,19 @@ import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import com.google.firebase.cloud.StorageClient;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 @Service
 public class ServiceCodeQR {
@@ -31,26 +52,46 @@ public class ServiceCodeQR {
 	public static final int OP_UPDATE=2;
 	
 	Firestore dbFirestore;
-
+	StorageClient storageClient;
+	Storage storage;
+	
 	// Method to generate Random ID DOCUMENT
 	public String autoIdDocument() {
 		return IDENTIFICATE + RandomStringUtils.randomAlphanumeric(ID_LENGTH);
 	}
 	
 	// Mapping the Object of the CodeQR class
-	private CodeQR mapCodeQR(CodeQR code, int option) {
+	private CodeQR mapCodeQR(CodeQR code, int option) throws WriterException, IOException {
+		
 		CodeQR qr = new CodeQR();
 		
-		qr.setGqr_code(code.getGqr_code());
-		qr.setGqr_description(code.getGqr_description());
-		qr.setGqr_image(code.getGqr_image());
-		qr.setGqr_registration_date(new Date().getTime());
-		qr.setGqr_asb_bus_id(code.getGqr_asb_bus_id());
-		
 		if (option == 1) {
+			System.out.println("ENTRO A LA OPCION[1] CREAR");
+			qr.setGqr_code(code.getGqr_code());
+			qr.setGqr_description(code.getGqr_description());
+			qr.setGqr_registration_date(new Date().getTime());
+			qr.setGqr_asb_bus_id(code.getGqr_asb_bus_id());
+			
+			byte [] imgQR = generatorQRCode(qr.getGqr_code(), 500, 500);
+			System.out.println("---->> BYTE IMG QR : " + imgQR);
+			
+			/*byte [] imgQR = generatorQRCode(qr.getGqr_code(), 500, 500);
+			// aqui jugamos con la imagen QR
+			Map<String, String> map = new HashMap();
+			map.put("firebaseStorageDownloadTokens", imgQR.toString());
+			
+			BlobId blobId = BlobId.of(bucket.toString(), imgQR.toString());
+			BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
+					.setMetadata(map)
+					.setContentType("qr/png")
+					.build();
+			storage.create(blobInfo);*/
+			qr.setGqr_image(code.getGqr_image());
 			// Mapping Object for Create (ESTADO)
 			qr.setGqr_status(true);
 		} if (option == 2) {
+			System.out.println("ENTRO A LA OPCION[2] ACTUALIZAR");
+			qr.setGqr_image(code.getGqr_image());
 			// Mapping Object for Update (ESTADO)
 			qr.setGqr_status(code.getGqr_status());
 		}
@@ -59,7 +100,7 @@ public class ServiceCodeQR {
 	}
 	
 	// Method to create new QR record
-	public String createQR(CodeQR qr) throws InterruptedException, ExecutionException {
+	public String createQR(CodeQR qr) throws InterruptedException, ExecutionException, WriterException, IOException {
 		
 		dbFirestore = FirestoreClient.getFirestore();
 		
@@ -184,5 +225,40 @@ public class ServiceCodeQR {
 		}else {
 			return null;
 		}
+	}
+	
+	private byte[] generatorQRCode(String code, int width, int height) throws WriterException, IOException {
+		
+		storageClient = StorageClient.getInstance();
+		
+		Bucket bucket = storageClient.bucket();
+		System.out.println(">>>>>>> BUCKET DE IMG QR : " + bucket);	
+		
+		String qcodePath = "C:/qr/" + code + ".png";
+		
+		QRCodeWriter qrCodeWriter = new QRCodeWriter();
+		
+		BitMatrix bitMatrix = qrCodeWriter.encode(code, BarcodeFormat.QR_CODE, width, height);
+		
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		Path pathC = FileSystems.getDefault().getPath(qcodePath);
+		
+		String qrIMG = code + ".png";
+		String blobImgQR = "imgQR/"+qrIMG;
+		String projectId = "practica1-busgeomap";
+		InputStream inputFile = new FileInputStream(qcodePath);
+		
+		MatrixToImageWriter.writeToStream(bitMatrix, "png", outputStream);
+		MatrixToImageWriter.writeToPath(bitMatrix, "png", pathC);
+		
+		// comenzando explorar API para guardar IMG
+		//BlobId blobId = BlobId.of(bucket.toString(), qrIMG);
+		//BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("img/qr").build();
+		//storage.create(blobInfo);
+		storageClient.bucket().create(blobImgQR, inputFile, Bucket.BlobWriteOption.userProject(projectId));
+		
+		byte[] qrData = outputStream.toByteArray();
+		
+		return qrData;
 	}
 }

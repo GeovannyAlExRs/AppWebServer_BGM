be2d221fcb8ce811b7417e54e6e96e67eba22189
Geovanny AlEx Rs <axel94bsc@gmail.com>
2022-01-31 23:42:21 -0500

    Modificaciones para agregar Datetable jquery

---
+++ AppWebServer_BGM/src/main/resources/static/js/datatableSchedule.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('#schList').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontro resultados",
+            "info": "Registro del _START_ al _END_ - Pagina _PAGES_",
+            "infoEmpty": "Registro del 0 al 0 - Pagina 0",
+            "infoFiltered": "(Registros total _MAX_)",
+            "sSearch": "Buscar: ",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Ultimo",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "sProcessing": "Procesando..."
+        }
+    });
+} );
